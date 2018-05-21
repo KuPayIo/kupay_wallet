18d296318c404d18d9c72bb649811675aca701cf
wup <460231340@qq.com>
2018-05-21 19:52:30 +0800

    FAIR-165 收款展示，地址二维码
    添加二维码组件

---
+++ src/pi/components/qrcode/qrcode.tpl
@@ -0,0 +1,3 @@
+<div>
+    <div style="width:100px; height:100px; margin-top:15px;margin-right: auto;margin-left: auto;"></div>
+</div>
---
+++ src/pi/components/qrcode/qrcode.ts
@@ -0,0 +1,34 @@
+/**
+ * 二维码组件
+ */
+import { Widget } from '../../widget/widget';
+import { notify } from '../../widget/event';
+import { QrcodeSrc } from './qrcode_src';
+import { getRealNode } from '../../widget/painter';
+
+interface Props {
+    value: string;
+}
+
+interface State {
+    circleProgress: string;
+
+}
+
+export class Qrcode extends Widget {
+    public props: Props;
+    constructor() {
+        super();
+    }
+
+    public firstPaint() {
+        let wrapper = <HTMLElement>getRealNode(this.tree);
+        console.log(this.tree, wrapper)
+        var qrcode: any = new QrcodeSrc(wrapper.children[0], {
+            width: 100,
+            height: 100
+        });
+        qrcode.makeCode(this.props.value);
+
+    }
+}
