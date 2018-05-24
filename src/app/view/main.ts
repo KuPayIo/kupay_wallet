b81910c586af89c3ebd0ef23e7addc1cfac42c2a
yuq <810824629@qq.com>
2018-05-24 20:07:07 +0800

    FAIR-193 首页页面调试

--- src/app/view/main.ts
+++ src/app/view/main.ts
@@ -20,8 +20,7 @@ export const WIDGET_NAME = module.id.replace(/\//g, '-');
 export const run = (): void => {
 	addWidget(document.body, 'pi-ui-root');
 	popNew('app-view-app');
-	//popNew('app-view-financialManagement-fund-home');
-	// popNew('app-view-languageAndcoinset-coinset');
+	popNew('app-view-wallet-walletCreate-walletCreate');
 };
 
 // ============================== 立即执行
