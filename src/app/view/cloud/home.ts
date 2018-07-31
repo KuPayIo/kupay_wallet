822308957fc9afb278c2bf99c6cd498b511b2f70
caozh <1217614362@qq.com>
2018-07-31 09:45:58 +0800

    FAIR-455 云账户界面与充值提现

---
+++ src/app/view/cloud/home.ts
@@ -0,0 +1,57 @@
+/**
+ * 云端首页
+ */
+import { popNew } from '../../../pi/ui/root';
+import { Widget } from '../../../pi/widget/widget';
+export class Home extends Widget {
+    constructor() {
+        super();
+    }
+    public create() {
+        super.create();
+        this.init();
+    }
+    public init(): void {
+        this.state = {
+            cloudBalance: '￥50.050',// 云端账户余额
+            exchangeRate: '0.000000109',// 兑换率
+            totalDeal: '2652125.624',// 交易量
+            totalDealYNC: '￥106,088.98',// 交易量换算人名币
+            increase: '-2.63%'// 涨跌幅
+        };
+    }
+
+    /**
+     * 点击云端账户
+     */
+    public cloudAccountClicked() {
+        // TODO
+        popNew('app-view-cloud-account-account');
+    }
+
+    /**
+     * 点击发红包
+     */
+    public packetsClicked() {
+        // TODO
+    }
+
+    /**
+     * 点击兑换领奖
+     */
+    public awardsClicked() {
+        // TODO
+    }
+    /**
+     * 领分红
+     */
+    public bonusClicked() {
+        // TODO
+    }
+    /**
+     * 点击邀请好友
+     */
+    public friendsClicked() {
+        // TODO
+    }
+}
