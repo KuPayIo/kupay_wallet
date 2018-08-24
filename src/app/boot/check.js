ff75dc75dba1d204fb00bffdd5ae41dc10fd3294
yineng <yineng@tom.com>
2018-08-24 18:29:12 +0800

    fix error that did not set flag back when network error occurred.

--- src/app/boot/check.js
+++ src/app/boot/check.js
@@ -155,15 +155,27 @@ function checkUpdate () {
                                     console.log('[newIndexJS]', newIndexJSversion);
 
                                     var forceUpdate = checkIfLargeUpdate(oldIndexJSVersion, newIndexJSversion);
+                                    var userAgreeUpdate = false;
                                     if (forceUpdate) {
                                         // to prevent user close application and restart application without network to avoid updates,
                                         // set pending flag before alert.
                                         localStorage.setItem("pending", "false");
                                         alert('A change on main version number is detected, update will start.');
                                     }
+                                    else {
+                                        userAgreeUpdate = confirm("Update detected, processed with update?");
+                                        
+                                        // If user disagree with update
+                                        if (!userAgreeUpdate) {
+                                            localized.setFetchFromLocal(function() {
+                                                console.log('current state: force-from-local');
+                                            }, function() { console.error('unable to set flag: fetch-from-local'); })
+                                        }
+                                    }
+                                    
 
                                     // version number changed, ask user to update
-                                    if (oldIndexJSVersion !== newIndexJSversion && (forceUpdate || confirm("Update detected, processed with update?"))) {
+                                    if (oldIndexJSVersion !== newIndexJSversion && (forceUpdate || userAgreeUpdate)) {
                                         localized.setForceFetchFromServer(function() {
                                             /// update the 5 files
                                             var updateFiles = [
@@ -213,8 +225,13 @@ function checkUpdate () {
                                     else {
                                         localized.setFetchFromLocal(function() {}, errCallback);
                                     }
-                                }, errCallback)
-                            }, function() { console.error('unable to set flag: force-fetch-from-local'); });
+                                }, function() {
+                                    // if get index.js failed, set flag back
+                                    localized.setFetchFromLocal(function() {
+                                        console.log('current state: force-from-local');
+                                    }, function() { console.error('unable to set flag: fetch-from-local'); })
+                                })
+                            }, function() { console.error('unable to set flag: force-fetch-from-server'); });
                         }, errCallback)
                     //}, errCallback);
             }, errCallback);
