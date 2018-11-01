365d12319dce1bfea65f043c901ad84b289a4cb5
limh <justsmileli@163.com>
2018-11-01 19:43:13 +0800

    FAIR-1063 多语言支持

---
+++ src/pi/ui/lang.ts
@@ -0,0 +1,21 @@
+/**
+ * 多语言组件
+ */
+
+// ============================== 导入
+import { Forelet } from "../widget/forelet";
+import { addLangListener } from "../util/lang";
+
+// ============================== 导出
+/**
+ * @description 导出组件Widget类
+ * @example
+ */
+export const forelet = new Forelet();
+
+// ============================== 本地
+
+// ============================== 立即执行
+addLangListener((langType:string)=>{
+	forelet.paint(langType);
+});
