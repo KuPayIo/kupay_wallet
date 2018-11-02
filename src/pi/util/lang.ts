3e2cb5e5d19fa92b97633c9cc98b815743e7a796
limh <justsmileli@163.com>
2018-11-02 15:10:35 +0800

    FAIR-1063 多语言支持

--- src/pi/util/lang.ts
+++ src/pi/util/lang.ts
@@ -7,7 +7,8 @@
  * @description 多语言字符串，必须由UI组件使用
  * @example
  */
-export class LangStr{
+export class LangStr {
+    // TODO
 }
 
 /**
@@ -15,29 +16,28 @@ export class LangStr{
  * @example
  */
 export const setLang = (str: any) => {
-	curLang = str;
-	for(let f of langListeners) {
-		f(str)
-	}
+    curLang = str;
+    for (const f of langListeners) {
+        f(str);
+    }
 };
 /**
  * @description 获得当前的显示语言
  * @example
  */
 export const getLang = ():string => {
-	return curLang;
+    return curLang;
 };
 /**
  * @description 添加语言改变监听器
  * @example
  */
 export const addLangListener = (cb: Function) => {
-	langListeners.push(cb);
+    langListeners.push(cb);
 };
 // ============================== 本地
 // 当前语言 cn en fr
-let curLang:string = "en";
+let curLang:string = 'en';
 
 // 语言改变监听器
-let langListeners:Function[] = [];
-
+const langListeners:Function[] = [];
