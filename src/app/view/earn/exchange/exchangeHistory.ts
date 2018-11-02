8c1de7e2eaa40d9f11e298cec06317ef4ea39cb9
limh <justsmileli@163.com>
2018-11-02 15:54:01 +0800

    FAIR-1063 多语言支持

--- src/app/view/earn/exchange/exchangeHistory.ts
+++ src/app/view/earn/exchange/exchangeHistory.ts
@@ -24,10 +24,12 @@ interface State {
     showMoreTips:boolean; // 是否显示底部加载更多提示
     convertNumber:number; // 兑换总数，不包含邀请红包
     convertNumberShow:number; // 兑换总数
-    isScroll:boolean;  // 页面是否滑动
+    scroll:boolean;  // 页面是否滑动
     inviteObj:any; // 邀请红包对象
     userList:any[]; // 用户信息列表
     cfgData:any; 
+    scrollHeight:number;
+    topRefresh:boolean; // 头部刷新按钮
 }
 
 export class ExchangeHistory extends Widget {
@@ -45,14 +47,16 @@ export class ExchangeHistory extends Widget {
             recordListShow:[],
             convertNumber:0,
             convertNumberShow:0,
-            isScroll:false,
+            scroll:false,
             start:undefined,
             refresh:true,
             hasMore:false, 
             showMoreTips:false, 
             inviteObj:null,
             userList:[],
-            cfgData:getLanguage(this)
+            cfgData:getLanguage(this),
+            scrollHeight:0,
+            topRefresh:false
         };
         this.initData();
         
@@ -180,6 +184,7 @@ export class ExchangeHistory extends Widget {
         const oh1 = document.getElementById('exchangeHistoryContent').offsetHeight;
         const oh2 = document.getElementById('exchangeHistoryRecords').offsetHeight;
         const scrollTop = document.getElementById('exchangeHistoryContent').scrollTop; 
+        this.state.scrollHeight = scrollTop;
         if (this.state.hasMore && this.state.refresh && (oh2 - oh1 - scrollTop) < 20) {
             this.state.refresh = false;
             console.log(this.state.cfgData.loading);
@@ -190,9 +195,9 @@ export class ExchangeHistory extends Widget {
         } 
 
         if (scrollTop > 0) {
-            this.state.isScroll = true;
+            this.state.scroll = true;
         } else {
-            this.state.isScroll = false;
+            this.state.scroll = false;
         }
         this.paint();
     }
