ed5b8d526a0e72fb92be27008cb4b214dc90fd59
zhaox <zhaoxin.1@qq.com>
2018-11-02 18:50:46 +0800

    fair-1049

--- src/app/view/earn/redEnvelope/writeRedEnv.ts
+++ src/app/view/earn/redEnvelope/writeRedEnv.ts
@@ -106,6 +106,7 @@ export class WriteRedEnv extends Widget {
      */
     public changeAmount(e: any) {
         if (this.state.showPin) {
+            this.state.oneAmount = e.value;
             this.state.totalAmount = e.value;
         } else {
             this.state.oneAmount = e.value;
@@ -150,7 +151,7 @@ export class WriteRedEnv extends Widget {
 
             return;
         }
-        if (this.state.oneAmount === 0 || this.state.totalAmount === 0) {
+        if (this.state.oneAmount === 0 && this.state.totalAmount === 0) {
             popNew('app-components-message-message', { content: this.state.cfgData.tips[1] });
 
             return;
@@ -176,7 +177,8 @@ export class WriteRedEnv extends Widget {
         }
 
         this.inputBlur();
-        const mess1 = this.state.cfgData.phrase[0] + this.state.oneAmount + curCoin.name +" / "+this.state.totalNum+ this.state.cfgData.phrase[1];
+        
+        const mess1 = this.state.cfgData.phrase[0] + this.state.totalAmount + curCoin.name +" / "+this.state.totalNum+ this.state.cfgData.phrase[1];
         // tslint:disable-next-line:max-line-length
         const mess2 = this.state.cfgData.phrase[2] + (this.state.showPin ? this.state.cfgData.redEnvType[1] : this.state.cfgData.redEnvType[0]);
         popNew('app-components-modalBoxInput-modalBoxInput', {
