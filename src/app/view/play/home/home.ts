cf2a10a1ed38530251fe1b0329c712e3f4486a08
limh <justsmileli@163.com>
2018-11-06 10:18:20 +0800

    FAIR-1059 玩和聊的头部修改

--- src/app/view/play/home/home.ts
+++ src/app/view/play/home/home.ts
@@ -7,6 +7,7 @@ import { popNew } from '../../../../pi/ui/root';
 import { Forelet } from '../../../../pi/widget/forelet';
 import { Widget } from '../../../../pi/widget/widget';
 import { openNewActivity } from '../../../logic/native';
+import { getUserInfo } from '../../../utils/tools';
 
 // ================================ 导出
 // tslint:disable-next-line:no-reserved-keywords
@@ -16,8 +17,17 @@ export const WIDGET_NAME = module.id.replace(/\//g, '-');
 export class PlayHome extends Widget {
     public ok: () => void;
     public props:Json = {
-        refresh:false
+        refresh:false,
+        avatar:'../../res/image1/default_avatar.png'
     };
+
+    public create() {
+        super.create();
+        const userInfo = getUserInfo();
+        if (userInfo) {
+            this.props.avatar = userInfo.avatar ? userInfo.avatar : '../../res/image1/default_avatar.png';
+        }
+    }
 
     public backPrePage() {
         this.ok && this.ok();
