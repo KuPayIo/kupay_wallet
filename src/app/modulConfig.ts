bf5d2b54514ab88ad08384277745ff8b023449e0
yuq <chinayu9iang@gmail.com>
2018-11-21 13:54:46 +0800

    FAIR-1070 台湾版

--- src/app/modulConfig.ts
+++ src/app/modulConfig.ts
@@ -4,11 +4,11 @@
 
 // ---------app模块功能配置----------------------------
 const appModulConfig = {
-    APP_CHAT: true,            // 聊天模块
+    APP_CHAT: false,            // 聊天模块
     APP_WALLET: true,           // 钱包模块
-    APP_EARN: true,             // 赚钱模块
+    APP_EARN: false,             // 赚钱模块
     APP_PLAY: true,            // 游戏模块
-    FINANCIAL_SERVICES: true,   // 优选理财
+    FINANCIAL_SERVICES: false,   // 优选理财
     GITHUB:true,                  // github显示
 
     WALLET_NAME: 'KuPay',           // 钱包名字
