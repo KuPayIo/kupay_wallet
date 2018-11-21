44ebf788cacdd8a8a755b6990b1acf1c7f5a337a
yuq <chinayu9iang@gmail.com>
2018-11-21 17:43:33 +0800

    FAIR-1070 cubo v0.1.12

--- src/app/modulConfig.ts
+++ src/app/modulConfig.ts
@@ -4,17 +4,17 @@
 
 // ---------app模块功能配置----------------------------
 const appModulConfig = {
-    APP_CHAT: true,            // 聊天模块
+    APP_CHAT: false,            // 聊天模块
     APP_WALLET: true,           // 钱包模块
-    APP_EARN: true,             // 赚钱模块
+    APP_EARN: false,             // 赚钱模块
     APP_PLAY: true,            // 游戏模块
-    FINANCIAL_SERVICES: true,   // 优选理财
-    GITHUB:true,                  // github显示
+    FINANCIAL_SERVICES: false,   // 优选理财
+    GITHUB:false,                  // github显示
 
-    WALLET_NAME: 'KuPay',           // 钱包名字
-    WALLET_WEBSITE:'www.kupay.io',     // 官网地址
+    WALLET_NAME: 'Cubo',           // 钱包名字
+    WALLET_WEBSITE:'www.Cubo.io',     // 官网地址
     LOGIN_IMG:'app/res/image/login_bg.png',  // 登录页面图片
-    WALLET_LOGO:'app/res/image/img_logo.png', // 钱包logo
+    WALLET_LOGO:'app/res/image/img_logo.jpg', // 钱包logo
     WECHAT_HELPER:'app/res/image/wechat_robot.jpg',  // 微信小助手二维码
     WECHAT_ACCOUNT:'app/res/image/wechat_pn.jpg'  // 微信公众号二维码
 };
