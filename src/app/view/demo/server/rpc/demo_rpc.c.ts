cb9fc453c5ca92aa6a04a5bf69d342a24e26b427
yuq <chinayu9iang@gmail.com>
2018-12-21 15:48:15 +0800

    FAIR-1231 修复钱包资源加载问题

--- src/app/view/demo/server/rpc/demo_rpc.c.ts
+++
@@ -1,7 +0,0 @@
-
-import {cfgMgr} from "../../../../../pi/util/cfg";
-import {Entrance} from "../../../../../pi_pt/entrance.s";
-
-let _$c = (path, notes):Entrance => {return new Entrance(path, notes)};
-let arr = [[0, _$c("app/view/demo/server/rpc/demo.demoSetUserInfo", new Map<string,string>([["rpc","rpcServer"]]))],[1, _$c("app/view/demo/server/rpc/demo.demoGetUserInfo", new Map<string,string>([["rpc","rpcServer"]]))]] as any;
-cfgMgr.update(Entrance._$info.name, new Map<number,any>(arr));
