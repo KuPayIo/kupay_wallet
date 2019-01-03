63d891d6e198032cf2f2fd691ac8315cc5f8bc3a
limh <justsmileli@163.com>
2019-01-03 13:30:05 +0800

    FAIR-1282 聊天首页面

--- src/app/view/base/app.ts
+++ src/app/view/base/app.ts
@@ -52,7 +52,7 @@ export class App extends Widget {
                     text: { zh_Hans:'聊',zh_Hant:'聊',en:'' },
                     icon: 'chat.png',
                     iconActive: 'chat_active.png',
-                    components: 'chat-client-app-demo_view-login-login'
+                    components: 'app-view-chat-home-home'
                 },
                 earn: {
                     modulName: 'APP_EARN',
