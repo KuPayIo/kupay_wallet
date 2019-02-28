d462114f90e1ee1d241180ed78a15d472f9b3d67
yuq <chinayu9iang@gmail.com>
2019-02-28 11:11:48 +0800

    FAIR-1740 更新  游客登录

--- src/app/boot/index.js
+++ src/app/boot/index.js
@@ -8,7 +8,7 @@
 document.body.style.backgroundColor="#2F2F2F";
 winit.path="/wallet/";//"/pi/0.1/";
 winit.loadJS(winit.getLoadDomain("init.js"), 
-	winit.path + 'app/boot/init.js?' + Math.random(), "utf8", winit.initFail, "load init error");
+	winit.path + 'pi/boot/init.js?' + Math.random(), "utf8", winit.initFail, "load init error");
 
 winit.loadJS(winit.getLoadDomain("next.js"), 
 	winit.path + 'app/boot/next.js?' + Math.random(), "utf8", winit.initFail, "load next error");
