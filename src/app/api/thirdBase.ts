527c4b549a6228273db8c50115c263e79dc7129c
yuq <chinayu9iang@gmail.com>
2019-03-25 13:04:31 +0800

    FAIR-2073 点击最小化，游戏化为悬浮窗，使用游戏logo做按钮图片

--- src/app/api/thirdBase.ts
+++ src/app/api/thirdBase.ts
@@ -1,5 +1,6 @@
 import { WebViewManager } from '../../pi/browser/webview';
 import { popNew } from '../../pi/ui/root';
+import { popNew3 } from '../utils/tools';
 import { getGameUrl } from '../view/play/home/gameConfig';
 
 /**
@@ -11,6 +12,7 @@ import { getGameUrl } from '../view/play/home/gameConfig';
   * 关闭打开的webview
   */
 export const closeWebview = (webViewName: string) => {
+    console.log('wallet closeWebview called');
     WebViewManager.close(webViewName);
 };
 
@@ -18,14 +20,17 @@ export const closeWebview = (webViewName: string) => {
  * 最小化webview
  */
 export const minWebview = (webViewName: string) => {
-    WebViewManager.minWebView(webViewName);
     console.log('wallet minWebview called');
+    popNew('app-components-floatBox-floatBox',{ webViewName });
+    WebViewManager.minWebView(webViewName);
+    
 };
 
 /**
  * 邀请好友
  */
 export const inviteFriends = (webViewName: string) => {
+    console.log('wallet inviteFriends called');
     popNew('earn-client-app-view-activity-inviteFriend',undefined,() => {
         WebViewManager.open(webViewName, `${getGameUrl(webViewName)}?${Math.random()}`, webViewName,'');
     });
@@ -36,8 +41,31 @@ export const inviteFriends = (webViewName: string) => {
  * 去充值
  */
 export const gotoRecharge = (webViewName: string) => {
+    console.log('wallet gotoRecharge called');
     popNew('app-view-wallet-cloudWalletGT-rechargeGT',undefined,() => {
         WebViewManager.open(webViewName, `${getGameUrl(webViewName)}?${Math.random()}`, webViewName,'');
     });
+    minWebview(webViewName);
+};
+
+/**
+ * 游戏客服
+ */
+export const gotoGameService = (webViewName: string) => {
+    console.log('wallet gotoGameService called');
+    // popNew('chat-client-app-view-chat-chat',{},() => {
+    //     WebViewManager.open(webViewName, `${getGameUrl(webViewName)}?${Math.random()}`, webViewName,'');
+    // });
+    minWebview(webViewName);
+};
+
+/**
+ * 官方群聊
+ */
+export const gotoOfficialGroupChat = (webViewName: string) => {
+    console.log('wallet gotoOfficialGroupChat called');
+    // popNew('chat-client-app-view-chat-chat',{},() => {
+    //     WebViewManager.open(webViewName, `${getGameUrl(webViewName)}?${Math.random()}`, webViewName,'');
+    // });
     minWebview(webViewName);
 };
