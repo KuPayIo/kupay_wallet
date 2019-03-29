aebe7dfb698f61a8aacc0be29f281d159ab59159
luob <lu_obin@foxmail.com>
2019-03-29 15:22:03 +0800

    # 支持https

--- scripts/mklink.bat
+++ scripts/mklink.bat
@@ -2,4 +2,5 @@ mklink /j "../src/pi" "../../pi/src/pi";
 mklink /j "../src/pi_pt" "../../pi_pt/src/pi_pt";
 mklink /j "../src/chat" "../../messaging/src/chat";
 mklink /j "../src/earn" "../../wallet_game/src/earn";
+mklink /j "../../node_modules" "../../pi_build/node_modules";
 pause;
